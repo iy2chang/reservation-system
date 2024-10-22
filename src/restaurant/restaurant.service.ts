34faaca38c3fcf2e79cc83bd1285c026d1109eea
Ian <ian.chang@flagship.cc>
2024-10-23 02:07:33 +0900

    add restaurant service

--- src/restaurant/restaurant.service.ts
+++ src/restaurant/restaurant.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Restaurant } from './schema/restaurant.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { RestaurantDto } from './dto/restaurant.dto';
 
 @Injectable()
-export class RestaurantService {}
+export class RestaurantService {
+  constructor(
+    @InjectModel(Restaurant.name) private restaurantModel: Model<Restaurant>,
+  ) {}
+
+  async create(restaurant: RestaurantDto): Promise<Restaurant> {
+    const createdRestaurant = await this.restaurantModel.create(restaurant);
+    return createdRestaurant;
+  }
+
+  async findAll(): Promise<Restaurant[]> {
+    const restaurants = await this.restaurantModel.find();
+    return restaurants;
+  }
+
+  async findOne(id: Types.ObjectId): Promise<Restaurant> {
+    const restaurant = await this.restaurantModel.findById(id).exec();
+    if (!restaurant) {
+      throw new NotFoundException(`Restaurant with id ${id} not found`);
+    }
+    return restaurant;
+  }
+
+  async update(
+    id: Types.ObjectId,
+    updateRestaurantDto: RestaurantDto,
+  ): Promise<Restaurant> {
+    const updatedRestaurant = await this.restaurantModel.findByIdAndUpdate(
+      id,
+      updateRestaurantDto,
+      { new: true },
+    );
+
+    if (!updatedRestaurant) {
+      throw new NotFoundException(`Restaurant with id ${id} not found`);
+    }
+    return updatedRestaurant;
+  }
+
+  async delete(id: Types.ObjectId): Promise<Restaurant> {
+    const result = await this.restaurantModel.findByIdAndDelete(id).exec();
+    if (!result) {
+      throw new NotFoundException(`Restaurant with ID ${id} not found`);
+    }
+    return result;
+  }
+}
