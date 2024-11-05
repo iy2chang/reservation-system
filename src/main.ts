89221fd49ada01bf897da29c839bb92dbb2c2331
Ian <ian.chang@flagship.cc>
2024-11-05 21:02:46 +0900

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -9,6 +10,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Reservation example')
+    .setDescription('The Reservation API description')
+    .setVersion('1.0')
+    .addTag('reservations')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
