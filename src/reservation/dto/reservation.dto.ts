6102fa228ab864156496cf781353e92f93b1e13b
Ian <ian.chang@flagship.cc>
2024-11-06 02:15:38 +0900

    add swagger post api property

--- src/reservation/dto/reservation.dto.ts
+++ src/reservation/dto/reservation.dto.ts
@@ -1,11 +1,14 @@
 import { Transform } from 'class-transformer';
 import { IsString, IsInt, Min, Max, IsDate, IsMongoId } from 'class-validator';
 import { Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ReservationDto {
+  @ApiProperty({ example: 'John Doe' })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 2, description: 'Number of Guests between 1 and 4' })
   @IsInt()
   @Min(1)
   @Max(4)
@@ -15,6 +18,10 @@ export class ReservationDto {
   @Transform(({ value }) => new Date(value))
   dateTime: Date;
 
+  @ApiProperty({
+    example: '60f1b0b3b3b3b3b3b3b3b3b3',
+    description: 'Restaurant ID',
+  })
   @IsMongoId()
   restaurant: Types.ObjectId;
 }
